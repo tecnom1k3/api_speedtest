352974b128010133c91c4d1201e10f60ffa644db
Angel Ibarra <1458333+tecnom1k3@users.noreply.github.com>
2025-07-23 11:47:47 -0700

    Add JSDoc comments to helper modules

--- modules/mysql.js
+++ modules/mysql.js
@@ -1,8 +1,20 @@
 "use strict";
 
+/**
+ * Factory for creating a MySQL connection instance.
+ * @module mysql
+ */
+
 const mysql = require("mysql");
 
+/**
+ * Provides access to the shared MySQL connection.
+ * @type {{connection: import("mysql").Connection}}
+ */
 const mysqlModule = ((mysql) => {
+    /**
+     * MySQL connection configured from environment variables.
+     */
     const connection = mysql.createConnection({
         host: process.env.RDS_HOST,
         user: process.env.RDS_USER,
--- modules/service/wake.js
+++ modules/service/wake.js
@@ -1,10 +1,23 @@
 "use strict";
 
+/**
+ * Helper functions for waking up the database connection.
+ * @module wake
+ */
+
 const winston = require("./../winston");
 const query = require("./../query");
 
+/**
+ * Collection of wake up service methods.
+ * @type {{wakeUp: function(): Promise<*>}}
+ */
 const wakeModule = ((winston, query) => {
 
+    /**
+     * Executes a trivial query to keep the database awake.
+     * @returns {Promise<*>} resolves when the query completes
+     */
     const wakeUp =  () => {
         winston.logger.info("waking up");
         return query.query("select 1 as wake", []);
