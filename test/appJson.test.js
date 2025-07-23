c1cfc3158547387597bf70a7060b54f31971765c
Angel Ibarra <1458333+tecnom1k3@users.noreply.github.com>
2025-07-23 15:45:16 -0700

    chore: apply eslint auto fixes

--- test/appJson.test.js
+++ test/appJson.test.js
@@ -2,30 +2,30 @@
  * Basic test verifying that JSON payloads are parsed when using only
  * Express's built-in middleware.
  */
-const express = require('express');
-const request = require('supertest');
-const proxyquire = require('proxyquire');
+const express = require("express");
+const request = require("supertest");
+const proxyquire = require("proxyquire");
 
 const stubRouter = express.Router();
-stubRouter.put('/', (req, res) => {
+stubRouter.put("/", (req, res) => {
     res.status(200).json({ parsed: req.body });
 });
 
-const app = proxyquire('../app', {
-    './routes/log': stubRouter
+const app = proxyquire("../app", {
+    "./routes/log": stubRouter
 });
 
 async function runTest() {
     await request(app)
-        .put('/log')
-        .send({ msg: 'hello' })
-        .expect(200, { parsed: { msg: 'hello' } });
+        .put("/log")
+        .send({ msg: "hello" })
+        .expect(200, { parsed: { msg: "hello" } });
 }
 
 runTest().then(() => {
-    console.log('JSON parsing test passed');
+    console.log("JSON parsing test passed");
 }).catch(err => {
-    console.error('JSON parsing test failed');
+    console.error("JSON parsing test failed");
     console.error(err);
     process.exit(1);
 });
--- test/logRoute.test.js
+++ test/logRoute.test.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const request = require('supertest');
-const proxyquire = require('proxyquire');
+const express = require("express");
+const request = require("supertest");
+const proxyquire = require("proxyquire");
 
 const winstonStub = { logger: { info: () => {}, error: () => {} } };
 
 async function runTests() {
     const successStub = { putLog: async () => 1 };
-    const logRouterSuccess = proxyquire('../routes/log', {
-        './../modules/log': successStub,
-        './../modules/winston': winstonStub
+    const logRouterSuccess = proxyquire("../routes/log", {
+        "./../modules/log": successStub,
+        "./../modules/winston": winstonStub
     });
     const appSuccess = express();
     appSuccess.use(express.json());
-    appSuccess.use('/', logRouterSuccess);
-    await request(appSuccess).put('/').send({}).expect(200);
+    appSuccess.use("/", logRouterSuccess);
+    await request(appSuccess).put("/").send({}).expect(200);
 
     const failStub = { putLog: async () => null };
-    const logRouterFail = proxyquire('../routes/log', {
-        './../modules/log': failStub,
-        './../modules/winston': winstonStub
+    const logRouterFail = proxyquire("../routes/log", {
+        "./../modules/log": failStub,
+        "./../modules/winston": winstonStub
     });
     const appFail = express();
     appFail.use(express.json());
-    appFail.use('/', logRouterFail);
-    await request(appFail).put('/').send({}).expect(409);
+    appFail.use("/", logRouterFail);
+    await request(appFail).put("/").send({}).expect(409);
 }
 
 runTests().then(() => {
-    console.log('Tests passed');
+    console.log("Tests passed");
 }).catch(err => {
-    console.error('Tests failed');
+    console.error("Tests failed");
     console.error(err);
     process.exit(1);
 });
