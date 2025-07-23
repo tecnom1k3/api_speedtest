e4e12ec273a6b25ad8ff2b12576f9b65a428dd98
Angel Ibarra <1458333+tecnom1k3@users.noreply.github.com>
2025-07-23 14:19:09 -0700

    Remove redundant body-parser usage and add JSON parsing test (#36)

---
+++ test/appJson.test.js
@@ -0,0 +1,32 @@
+/**
+ * Basic test verifying that JSON payloads are parsed when using only
+ * Express's built-in middleware.
+ */
+const express = require('express');
+const request = require('supertest');
+const proxyquire = require('proxyquire');
+
+const stubRouter = express.Router();
+stubRouter.put('/', (req, res) => {
+    res.status(200).json({ parsed: req.body });
+});
+
+const app = proxyquire('../app', {
+    './routes/log': stubRouter,
+    'serverless-http': app => app
+}).handler;
+
+async function runTest() {
+    await request(app)
+        .put('/log')
+        .send({ msg: 'hello' })
+        .expect(200, { parsed: { msg: 'hello' } });
+}
+
+runTest().then(() => {
+    console.log('JSON parsing test passed');
+}).catch(err => {
+    console.error('JSON parsing test failed');
+    console.error(err);
+    process.exit(1);
+});
